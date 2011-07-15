fcfc7d2a6c8aab3b2a4d256d693f755ed7822b11
Marco Herrero <hielo.helado@gmail.com>
2011-07-15 14:27:46 +0200

    First commit, vesion 0.1 finalizada

---
+++ src/me/helmetk/BedTime/BedTime.java
@@ -0,0 +1,60 @@
+package me.helmetk.BedTime;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.logging.Logger;
+
+import org.bukkit.command.*;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class BedTime extends JavaPlugin {
+	Logger log = Logger.getLogger("Minecraft"); 
+	public void onEnable(){
+		log.info("BedTime has been enabled");
+	}
+	public void onDisable(){
+		log.info("BedTime has been disabled");
+	}
+	public boolean onCommand(	CommandSender sender,
+								Command cmd, 
+								String commandLabel, 
+								String[] args){
+		// Si el jugador escribe el comando /bed hace el doSomething
+		
+		Player player = (Player) sender;
+		
+		if(cmd.getName().equalsIgnoreCase("bed")){
+			Player[] players = this.getServer().getOnlinePlayers();
+			List<Player> bed = new LinkedList<Player>();
+			List<Player> nobed = new LinkedList<Player>();
+
+			for(Player p:players){
+				if(p.isSleeping())
+					bed.add(p);
+				else
+					nobed.add(p);
+					
+			}
+			//Los strings
+			String stBed = "";
+			for(Player p:bed){
+				stBed = stBed + p.getDisplayName() + ",  ";
+			}
+			String stNoBed = "";
+			for(Player p:nobed){
+				stNoBed = stNoBed + p.getDisplayName() + ",  ";
+			}
+			player.sendMessage("[BedTime]");
+			player.sendMessage("Bed:");
+			player.sendMessage(stBed);
+			player.sendMessage("No Bed:");
+			player.sendMessage(stNoBed);
+			bed.clear();
+			nobed.clear();
+			return true;
+		}
+		
+		return false;
+	}
+}
